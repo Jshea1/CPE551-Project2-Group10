9c33e5e188d80d8101f2193c806c8d1b33d800a5
jshea1 <jshea1@stevens.edu>
2024-05-02 16:12:06 -0400

    Added components to last three  tabs

--- src/RecommenderGUI.py
+++ src/RecommenderGUI.py
@@ -51,8 +51,8 @@ class Recommender:
             return "No book data loaded yet"
         maxTitleLength = max(len(title) for title, _ in self._books)
         header = f"{'Title'.ljust(maxTitleLength + 4)}Author"
-        formatted_books = [f"{title.ljust(maxTitleLength + 4)}{author}" for title, author in self._books]
-        return header + "\n" + "\n".join(formatted_books)
+        booksTabFormat = [f"{title.ljust(maxTitleLength + 4)}{author}" for title, author in self._books]
+        return header + "\n" + "\n".join(booksTabFormat)
 
 
 class RecommenderGUI:
@@ -66,25 +66,35 @@ class RecommenderGUI:
         self._notebook = ttk.Notebook(self._rootWindow)
         self._notebook.pack(expand=True, fill="both")
 
-        self._notebook_tab = ttk.Frame(self._notebook)
-        self._notebook.add(self._notebook_tab, text="Movies")
+        # Movies tab begins
 
-        self._movieText = tk.Text(self._notebook_tab, height=10, wrap='word', state='disabled')
+        self._moviesTab = ttk.Frame(self._notebook)
+        self._notebook.add(self._moviesTab, text="Movies")
+
+        self._movieText = tk.Text(self._moviesTab, height=10, wrap='word', state='disabled')
         self._movieText.pack(padx=10, pady=10, fill='both', expand=True)
 
-        self._scrollbar = tk.Scrollbar(self._notebook_tab, command=self._movieText.yview)
+        self._scrollbar = tk.Scrollbar(self._moviesTab, command=self._movieText.yview)
         self._scrollbar.pack(side='right', fill='y')
         self._movieText.config(yscrollcommand=self._scrollbar.set)
 
-        self._tv_shows_tab = ttk.Frame(self._notebook)
-        self._notebook.add(self._tv_shows_tab, text="TV Shows")
+        # Movies tab ends
 
-        self._tvShowText = tk.Text(self._tv_shows_tab, height=10, wrap='word', state='disabled')
+        # TV tab begins
+
+        self._tvTab = ttk.Frame(self._notebook)
+        self._notebook.add(self._tvTab, text="TV Shows")
+
+        self._tvShowText = tk.Text(self._tvTab, height=10, wrap='word', state='disabled')
         self._tvShowText.pack(padx=10, pady=10, fill='both', expand=True)
 
-        self._tv_scrollbar = tk.Scrollbar(self._tv_shows_tab, command=self._tvShowText.yview)
-        self._tv_scrollbar.pack(side='right', fill='y')
-        self._tvShowText.config(yscrollcommand=self._tv_scrollbar.set)
+        self._tvScrollbar = tk.Scrollbar(self._tvTab, command=self._tvShowText.yview)
+        self._tvScrollbar.pack(side='right', fill='y')
+        self._tvShowText.config(yscrollcommand=self._tvScrollbar.set)
+
+        # TV tab ends
+
+        # Books tab begins
 
         self._booksTab = ttk.Frame(self._notebook)
         self._notebook.add(self._booksTab, text="Books")
@@ -96,14 +106,111 @@ class RecommenderGUI:
         self._booksScrollbar.pack(side='right', fill='y')
         self._bookText.config(yscrollcommand=self._booksScrollbar.set)
 
-        self._tv_shows_tab = ttk.Frame(self._notebook)
-        self._notebook.add(self._tv_shows_tab, text="Search Movies/TV")
+        # Books tab ends
 
-        self._tv_shows_tab = ttk.Frame(self._notebook)
-        self._notebook.add(self._tv_shows_tab, text="Search Books")
+        # Search Movies/TV tab begins
 
-        self._tv_shows_tab = ttk.Frame(self._notebook)
-        self._notebook.add(self._tv_shows_tab, text="Recommendations")
+        self._searchMoviesTab = ttk.Frame(self._notebook)
+        self._notebook.add(self._searchMoviesTab, text="Search Movies/TV")
+
+        self._TypeLabel = ttk.Label(self._searchMoviesTab, text="Type:")
+        self._TypeLabel.grid(row=0, column=0, padx=0, pady=5)
+        self._typeCombobox = ttk.Combobox(self._searchMoviesTab, values=["Movie", "TV Show"])
+        self._typeCombobox.grid(row=0, column=1, padx=0, pady=10)
+        self._typeCombobox.set("Movie")  # Default value
+
+        self._titleLabel = ttk.Label(self._searchMoviesTab, text="Title:")
+        self._titleLabel.grid(row=1, column=0, padx=10, pady=5)
+        self._titleEntry = ttk.Entry(self._searchMoviesTab)
+        self._titleEntry.grid(row=1, column=1, padx=10, pady=5)
+
+        self._directorLabel = ttk.Label(self._searchMoviesTab, text="Director:")
+        self._directorLabel.grid(row=2, column=0, padx=10, pady=5)
+        self._directorEntry = ttk.Entry(self._searchMoviesTab)
+        self._directorEntry.grid(row=2, column=1, padx=10, pady=5)
+
+        self._actorLabel = ttk.Label(self._searchMoviesTab, text="Actor:")
+        self._actorLabel.grid(row=3, column=0, padx=10, pady=5)
+        self._actorEntry = ttk.Entry(self._searchMoviesTab)
+        self._actorEntry.grid(row=3, column=1, padx=10, pady=5)
+
+        self._genreLabel = ttk.Label(self._searchMoviesTab, text="Genre:")
+        self._genreLabel.grid(row=4, column=0, padx=10, pady=5)
+        self._genreEntry = ttk.Entry(self._searchMoviesTab)
+        self._genreEntry.grid(row=4, column=1, padx=10, pady=5)
+
+        self._searchButton = ttk.Button(self._searchMoviesTab, text="Search")
+        self._searchButton.grid(row=5, column=0, columnspan=2, pady=10)
+
+        self._textResults = tk.Text(self._searchMoviesTab, height=30, wrap='word', state='disabled')
+        self._textResults.grid(row=6, column=0, columnspan=15, padx=5, pady=10)
+
+        self._resultsScrollbar = ttk.Scrollbar(self._searchMoviesTab, command=self._textResults.yview)
+        self._resultsScrollbar.grid(row=6, column=15)
+        self._textResults.config(yscrollcommand=self._resultsScrollbar.set)
+
+        # Search Movies/TV tab begins
+
+        # Search books tab begins
+
+        self._searchBooksTab = ttk.Frame(self._notebook)
+        self._notebook.add(self._searchBooksTab, text="Search Books")
+
+        self._titleLabel = ttk.Label(self._searchBooksTab, text="Title:")
+        self._titleLabel.grid(row=1, column=0, padx=10, pady=5)
+        self._titleEntry = ttk.Entry(self._searchBooksTab)
+        self._titleEntry.grid(row=1, column=1, padx=10, pady=5)
+
+        self._directorLabel = ttk.Label(self._searchBooksTab, text="Author:")
+        self._directorLabel.grid(row=2, column=0, padx=10, pady=5)
+        self._directorEntry = ttk.Entry(self._searchBooksTab)
+        self._directorEntry.grid(row=2, column=1, padx=10, pady=5)
+
+        self._actorLabel = ttk.Label(self._searchBooksTab, text="Publisher:")
+        self._actorLabel.grid(row=3, column=0, padx=10, pady=5)
+        self._actorEntry = ttk.Entry(self._searchBooksTab)
+        self._actorEntry.grid(row=3, column=1, padx=10, pady=5)
+
+        self._searchButton = ttk.Button(self._searchBooksTab, text="Search")
+        self._searchButton.grid(row=5, column=0, columnspan=2, pady=10)
+
+        self._textResults = tk.Text(self._searchBooksTab, height=30, wrap='word', state='disabled')
+        self._textResults.grid(row=6, column=0, columnspan=15, padx=5, pady=10)
+
+        self._resultsScrollbar = ttk.Scrollbar(self._searchBooksTab, command=self._textResults.yview)
+        self._resultsScrollbar.grid(row=6, column=15)
+        self._textResults.config(yscrollcommand=self._resultsScrollbar.set)
+
+        # Search books tab ends
+
+        # Recommendations tab begins
+
+        self._recommendationsTab = ttk.Frame(self._notebook)
+        self._notebook.add(self._recommendationsTab, text="Recommendations")
+
+        self._titleLabel = ttk.Label(self._recommendationsTab, text="Type:")
+        self._titleLabel.grid(row=1, column=0, padx=10, pady=5)
+        self._titleEntry = ttk.Entry(self._recommendationsTab)
+        self._titleEntry.grid(row=1, column=1, padx=10, pady=5)
+
+        self._directorLabel = ttk.Label(self._recommendationsTab, text="Title:")
+        self._directorLabel.grid(row=2, column=0, padx=10, pady=5)
+        self._directorEntry = ttk.Entry(self._recommendationsTab)
+        self._directorEntry.grid(row=2, column=1, padx=10, pady=5)
+
+        self._searchButton = ttk.Button(self._recommendationsTab, text="Get recommendation")
+        self._searchButton.grid(row=5, column=0, columnspan=2, pady=10)
+
+        self._textResults = tk.Text(self._recommendationsTab, height=30, wrap='word', state='disabled')
+        self._textResults.grid(row=6, column=0, columnspan=15, padx=5, pady=10)
+
+        self._resultsScrollbar = ttk.Scrollbar(self._recommendationsTab, command=self._textResults.yview)
+        self._resultsScrollbar.grid(row=6, column=15)
+        self._textResults.config(yscrollcommand=self._resultsScrollbar.set)
+
+        # Recommendations tab ends
+
+        # Bottom button code begins
 
         self._buttonFrame = tk.Frame(self._rootWindow)
         self._buttonFrame.pack(side='bottom', fill='x', padx=10, pady=10)
