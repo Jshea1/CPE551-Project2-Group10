5df755249fcec02240d524a21cb69e43fb1f5f67
Yash <123642742+yashp26@users.noreply.github.com>
2024-05-03 12:17:27 -0400

    Update Book.py

--- src/Book.py
+++ src/Book.py
@@ -1,50 +1,62 @@
-# Author: Saurabh Parkar
-
 from Media import Media
 
 class Books(Media):
-    def __init__(self, id: str, title: str, rating: float, author: str, isbn: float,isbn13: float, numOfRatings: float, pubDate: str, publisher: str):
+    def __init__(self, id: str, title: str, rating: float, author: str, isbn: float, isbn13: float, numOfRatings: float, pubDate: str, publisher: str, languageCode: str, numPages: int):
         self.__author = author
         self.__isbn = isbn
         self.__isbn13 = isbn13
         self.__numOfRatings = numOfRatings
         self.__pubDate = pubDate
         self.__publisher = publisher
+        self.__languageCode = languageCode
+        self.__numPages = numPages
 
-        super().__init__(id,title,rating)
-    
-    def author(self):
+        super().__init__(id, title, rating)
+
+    def getAuthor(self):
         return self.__author
 
-    def author(self, newAuthor):
+    def setAuthor(self, newAuthor):
         self.__author = newAuthor
 
-    def isbn(self):
+    def getIsbn(self):
         return self.__isbn
 
-    def isbn(self, newIsbn):
+    def setIsbn(self, newIsbn):
         self.__isbn = newIsbn
 
-    def isbn13(self):
+    def getIsbn13(self):
         return self.__isbn13
 
-    def isbn13(self, newIsbn13):
+    def setIsbn13(self, newIsbn13):
         self.__isbn13 = newIsbn13
 
-    def numOfRatings(self):
+    def getNumOfRatings(self):
         return self.__numOfRatings
 
-    def numOfRatings(self, newNumOfRatings):
+    def setNumOfRatings(self, newNumOfRatings):
         self.__numOfRatings = newNumOfRatings
 
-    def pubDate(self):
+    def getPubDate(self):
         return self.__pubDate
 
-    def pubDate(self, newPubDate):
+    def setPubDate(self, newPubDate):
         self.__pubDate = newPubDate
 
-    def publisher(self):
+    def getPublisher(self):
         return self.__publisher
-    
-    def publisher(self, newPublisher):
+
+    def setPublisher(self, newPublisher):
         self.__publisher = newPublisher
+
+    def getLanguageCode(self):
+        return self.__languageCode
+
+    def setLanguageCode(self, newLanguageCode):
+        self.__languageCode = newLanguageCode
+
+    def getNumPages(self):
+        return self.__numPages
+
+    def setNumPages(self, newNumPages):
+        self.__numPages = newNumPages
